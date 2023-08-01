23acd1736b3d27fe962f2ae34dffb3d7ffbb53a3
Chaeyeon Seo <ssallyseo@gmail.com>
2023-08-01 19:30:23 +0900

    풀이 보완

--- July/0723.py
+++ July/0723.py
@@ -5,7 +5,7 @@
 
 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning, 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 매개변수로 주어질 때, 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return하는 solution 함수를 완성해주세요.
 '''
-
+# 1차 풀이
 def solution(name, yearning, photo):
     answer = []
     yearning_dict = {name[i]:yearning[i] for i in range(len(name))}
@@ -15,6 +15,12 @@ def solution(name, yearning, photo):
             if key in yearning_dict:
                 score+= yearning_dict[key]
         answer.append(score)
+    return answer
+
+# 2차 풀이
+def solution(name, yearning, photo):
+    yearning_dict = dict(zip(name, yearning))
+    answer = [sum(yearning_dict.get(key, 0) for key in lst) for lst in photo]
     return answer
 
 
