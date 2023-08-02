54d2590f8e51d4b59493c5ebededda989c82e3be
Chaeyeon Seo <ssallyseo@gmail.com>
2023-08-02 18:51:54 +0900

    0802 해결

--- Aug/0802.py
+++ Aug/0802.py
@@ -1,1 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142086
+
+def solution(s):
+    answer = []
+    for i in range(len(s)):
+        find_one = s[:i].rfind(s[i])
+        if find_one >= 0:
+            answer.append(len(s[:i]) - find_one)
+        else:
+            answer.append(find_one)
+    return answer
+
+s1 = "banana" # [-1, -1, -1, 2, 2, 2]
+s2 = "foobar" # [-1, -1, 1, -1, -1, -1]
+
+solution(s1)
+solution(s2)
