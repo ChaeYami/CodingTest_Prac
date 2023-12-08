a3e2e652c01a1232e331dbe7c863478e7c9f7d4b
Chaeyeon Seo <ssallyseo@gmail.com>
2023-12-08 20:14:14 +0900

    1208update

--- Dec/1208(1).py
+++ Dec/1208(1).py
@@ -30,7 +30,8 @@ def DFS(k, complete, dungeons, check):
     global answer
     answer = max(answer, complete) # 탐험 수가 크면 업데이트
     for i in range(len(dungeons)):
-	    # 방문하지 않은 던전이고, 현재 피로도가 해당 던전을 방문하기 위한 최소 피로도보다 크면
+        
+	    # 방문하지 않은 던전이고, 최소 피로도보다 크면
         if check[i] == 0 and k >= dungeons[i][0]:
 	        # 방문처리
             check[i] = 1
@@ -39,11 +40,9 @@ def DFS(k, complete, dungeons, check):
             check[i] = 0
 
 def solution(k, dungeons):
-    # answer = 0
     global answer
-    check = [0]*len(dungeons) # 방문 여부
     
-    # complete: 탐험한 던전 개수, k: 남은 피로도
-    DFS(k, 0, dungeons, check) # 0: 방문한 던전의 개수를 0으로 DFS 함수에 넘겨준다.
+    check = [0]*len(dungeons) # 방문 여부
+    DFS(k, 0, dungeons, check) # 0: 방문한 던전의 개수를 0으로
     
     return answer
