5457fd5574616ca095c19a354e49e69191e17a3f
Chaeyeon Seo <ssallyseo@gmail.com>
2023-12-28 23:28:22 +0900

    test

--- Dec_2023/[BAEKJOON] 1228-15652.py
+++ Dec_2023/[BAEKJOON] 1228-15652.py
@@ -9,6 +9,7 @@ N = lst[0]
 M = lst[1]
 
 numbers = list(range(1, N + 1))
+
 combinations_list = list(itertools.combinations_with_replacement(numbers, M)) # 중복조합
 
 for pair in [' '.join(map(str, combi)) for combi in combinations_list]:
