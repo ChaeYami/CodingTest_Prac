4ade768425d15975f14a7b5f9862a266a74bcde3
Chaeyeon Seo <ssallyseo@gmail.com>
2023-12-29 01:35:01 +0900

    🛠️README 소스 수정

--- utils/count_problem_source.py
+++ utils/count_problem_source.py
@@ -1,28 +1,35 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from datetime import datetime
 import os
 import re
 
 def count_problem_source_code():
-    month_files = []
     
     problem_solve_code_list = []
 
     directory_list = [directory for directory in os.listdir("./") if "2023" in directory or "2024" in directory]
 
+    file_count_by_month_year = defaultdict(int)
+    
     for directory in directory_list:
         code_list = os.listdir(f"./{directory}")
         
         # 사이트별
-
         problem_solve_code_list += code_list
+        
+        
+        #날짜별
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
 
-        # 날짜별 (폴더명 = 달)
-        file_count = len(code_list)
-        temp = [directory, file_count]
-        month_files.append(temp)
-        
-        
+                creation_date = datetime.utcfromtimestamp(os.path.getctime(file_path))
+
+                month_year_key = creation_date.strftime("%m-%Y")
+
+                file_count_by_month_year[month_year_key] += 1
+
+    # 사이트별
     name_list = [re.findall(r'\[[^)]*\]', code_name) for code_name in problem_solve_code_list]
 
     name_list = [name[0].replace("[", "").replace("]", "") for name in name_list if len(name) > 0]
@@ -31,8 +38,10 @@ def count_problem_source_code():
     
     code_cnt_info = sorted(Counter(name_list).items(), key=lambda x: -x[1])
     
-    month_files_info = sorted(month_files)
-
+    
+    # 날짜별
+    month_files_info = sorted(file_count_by_month_year.items(), key=lambda x: (int(x[0][3:]), int(x[0][:2])))
+    
     return total_code_num, code_cnt_info, month_files_info
 
 
@@ -47,9 +56,13 @@ def make_count_site_info(total_code_num, code_cnt_info):
 
 def make_count_month_info(month_files_info):
     month_info = f"### 날짜별 해결한 문제 \n"
-    for directory_month_files_info in month_files_info:
-        temp = f"#### {directory_month_files_info[0]} : {directory_month_files_info[1]}개\n"
+    
+    for month_year, count in month_files_info:
+        formatted_month_year = datetime.strptime(month_year, "%m-%Y").strftime("%b-%Y")
+        
+        temp = f"#### {formatted_month_year} : {count}개\n"
         month_info += temp
+        
     return month_info
 
 
